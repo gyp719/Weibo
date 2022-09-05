6a734d68b0335c189fa238892725d7b4e949beba
gyp719 <836230540@qq.com>
2022-09-05 19:26:06 +0900

    分页

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -21,7 +21,7 @@ class UsersController extends Controller
 
     public function index()
     {
-        $users = User::all();
+        $users = User::query()->paginate(6);
 
         return view('users.index', compact('users'));
     }
