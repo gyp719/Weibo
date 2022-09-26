0741a21dc5e6faa78f6f83daba2d609236effc25
gyp719 <836230540@qq.com>
2022-09-26 17:30:28 +0900

    关注和粉丝列表页面

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,6 @@ Route::get('password/reset/{token}',  'PasswordController@showResetForm')->name(
 Route::post('password/reset',  'PasswordController@reset')->name('password.update');
 
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings'); // 关注人列表
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers'); // 粉丝列表
